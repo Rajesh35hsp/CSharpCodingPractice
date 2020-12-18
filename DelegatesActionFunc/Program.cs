f277839ea40551853c12c92cf377f49eeaba99cb
rajeshkumargup <rajeshkumar.gupta@basware.com>
2020-12-18 22:47:03 +0530

    delegate and func

--- DelegatesActionFunc/Program.cs
+++ DelegatesActionFunc/Program.cs
@@ -2,11 +2,40 @@
 
 namespace DelegatesActionFunc
 {
+    //declaring custom delegate as we do declare class
+    public delegate int myCustomDelForCal(int a, int b);
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Action a = PrintSomeThing;
+            
+            calculator(5, 10, Sum);
+
+            myCustomDelForCal s = Sum;
+            calculatorGeneric(2, 3, s);
+        }
+
+        private static void calculatorGeneric(int a, int b, myCustomDelForCal s)
+        {
+            Console.WriteLine(s(a, b));
+
+        }
+
+        public static void calculator(int a, int b, Func<int, int, int> oper)
+        {
+            Console.WriteLine(oper(a, b));
+        }
+
+        public static void PrintSomeThing()
+        {
+            Console.WriteLine("hello");
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
         }
     }
 }
