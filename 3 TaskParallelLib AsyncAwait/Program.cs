cc1d27bc208aed674613d35b0a56159a19f874f1
rajeshkumargup <rajeshkumar.gupta@basware.com>
2021-07-15 11:06:17 +0530

    some old

---
+++ 3 TaskParallelLib AsyncAwait/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _3_TaskParallelLib_AsyncAwait
+{
+    class Program
+    {
+        static bool done = false;
+        static void Main(string[] args)
+        {
+            try
+            {
+                var e = DoSomeTimeConsumingAsyn();
+              //  while (!done)
+                {
+                    Console.WriteLine(System.DateTime.Now);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+
+        static async Task DoSomeTimeConsumingAsyn()
+        {
+            try
+            {
+                await Task.Run(() => {
+                    Thread.Sleep(5000);
+                    Console.WriteLine("sm");
+                    throw new Exception();
+                });
+            }
+            catch (Exception)
+            {
+                done = true;
+
+                throw;
+            }
+
+        }
+
+
+    }
+}
---
+++ 4 test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
